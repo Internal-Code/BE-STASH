84e000c2f6aa13a1369211a11faca6d3f0895cb7
armananta <armanantabastian@gmail.com>
2024-05-27 09:12:46 +0700

    feature(create_schema): fix environment variable to database connection

--- api/database/databaseConnection.py
+++ api/database/databaseConnection.py
@@ -1,5 +1,4 @@
 from loguru import logger
-from typing import Union
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.engine.base import Engine
 from api.secret import (
@@ -20,18 +19,17 @@ def database_connection(
     LOCAL_POSTGRESQL_DATABASE: str = LOCAL_POSTGRESQL_DATABASE,
     LOCAL_POSTGRESQL_POOL_SIZE: int = LOCAL_POSTGRESQL_POOL_SIZE,
     LOCAL_POSTGRESQL_MAX_OVERFLOW: int = LOCAL_POSTGRESQL_MAX_OVERFLOW
-) -> Union[Engine, None]:
+) -> Engine | None:
 
-    engine = None
-    
     try:
         engine = create_async_engine(
             f"postgresql+asyncpg://{LOCAL_POSTGRESQL_USER}:{LOCAL_POSTGRESQL_PASSWORD}@{LOCAL_POSTGRESQL_HOST}:{LOCAL_POSTGRESQL_PORT}/{LOCAL_POSTGRESQL_DATABASE}",
-            pool_size = LOCAL_POSTGRESQL_POOL_SIZE,
-            max_overflow = LOCAL_POSTGRESQL_MAX_OVERFLOW
+            pool_size = int(LOCAL_POSTGRESQL_POOL_SIZE),
+            max_overflow = int(LOCAL_POSTGRESQL_MAX_OVERFLOW)
         )
         
     except Exception as E:
+        engine = None
         logger.error(f"Error connecting to PostgreSQL: {E}")
     
     return engine
