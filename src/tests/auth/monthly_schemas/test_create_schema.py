b085c480c01c77bfce5ee44c78409d4342b045b2
armananta <armanantabastian@gmail.com>
2024-07-21 18:47:22 +0700

    feature(account_management): fix test_create_schema unit testing

--- src/tests/auth/monthly_schemas/test_create_schema.py
+++ src/tests/auth/monthly_schemas/test_create_schema.py
@@ -1,33 +1,48 @@
-import pytest
-import httpx
-from src.database.models import money_spend_schemas
-from src.database.connection import database_connection
-from sqlalchemy import select
+import pytest, httpx
+from jose import jwt
+from datetime import datetime
+from src.auth.utils.database.general import create_category_format
+from src.auth.utils.generator import random_number, random_word
+from src.secret import ACCESS_TOKEN_SECRET_KEY, ACCESS_TOKEN_ALGORITHM
 
-@pytest.fixture(scope="session")
-def db_data():
-    with database_connection().connect() as session:
-        with session.begin():
-            res = session.execute(select(money_spend_schemas))
-            col = res.keys()
-            rows = res.fetchall()
-            result = []
+TEST_USERNAME = "string"
+TEST_PASSWORD = "String123!"
 
-            for row in rows:
-                row_dict = {col: getattr(row, col) for col in col}
-                result.append(row_dict)
-
-        session.close()
-    return result
-
-        
 @pytest.mark.asyncio
-async def test_validate_create_schema(db_data):
+async def test_create_schema():
     """
-    Should return forbidden if any same schema inside the table.
+    Should return 201 for creating new schema.
     """
-    sample_data = db_data[0]
     async with httpx.AsyncClient() as client:
-        res = await client.post("http://localhost:8000/api/v1/create_schema", json=sample_data)
-        assert res.status_code == 403
+        login_data = {
+            "username": TEST_USERNAME,
+            "password": TEST_PASSWORD
+        }
+        
+        token_response = await client.post("http://localhost:8000/auth/token", data=login_data)
+        assert token_response.status_code == 200
+        tokens = token_response.json()
+        access_token = tokens["access_token"]
+        decoded_token = jwt.decode(access_token, ACCESS_TOKEN_SECRET_KEY, algorithms=[ACCESS_TOKEN_ALGORITHM])
+        user_uuid = decoded_token["user_uuid"]
+        
+        data = create_category_format(
+            user_uuid=user_uuid,
+            month=random_number(), 
+            year=random_number(4), 
+            category=f'testing-{random_word()}', 
+            budget=random_number(10)
+        )
+        
+        for key, value in data.items():
+            if isinstance(value, datetime):
+                data[key] = value.isoformat()
+        
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        
+        schema_response = await client.post("http://localhost:8000/api/v1/create-schema", json=data, headers=headers)
+        
+        assert schema_response.status_code == 201
         await client.aclose()
