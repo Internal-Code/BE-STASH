e36de6da9a57767d516c67260bdad279e100667e
bastian-arman <putra.armananta@gmail.com>
2025-01-14 16:28:05 +0700

    chore: refactored send reset link endpoint.

--- src/schema/response.py
+++ src/schema/response.py
@@ -4,19 +4,21 @@ from typing import Optional, Any
 
 class ResponseDefault(BaseModel):
     success: bool = True
-    message: str = None
+    message: Optional[str] = None
     data: Optional[Any] = None
 
 
 class ResponseToken(BaseModel):
-    access_token: str = None
-    refresh_token: str = None
-    token_type: str = "Bearer"
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
+    token_type: Optional[str] = "Bearer"
 
 
 class UniqueId(BaseModel):
-    unique_id: str = None
+    unique_id: Optional[str] = None
 
 
 class UserStatus(UniqueId):
-    register_status: str = None
+    register_status: Optional[str] = None
+    verified_phone_number: bool = False
+    verified_email: bool = False
